58bcbaad6e35e542eb9ff9a8301051655458884e
Nikita Glazkov <nik.glazkov2016@yandex.ru>
2023-09-29 16:05:23 +0300

    Add CustoEnumerator

---
+++ CustomEnumerator/Garage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEnumerator
+{
+    internal class Garage : IEnumerable
+    {
+        private Car[] carArray = new Car[4];
+
+        public Garage()
+        {
+            carArray[0] = new Car("Rusty",30);
+            carArray[1] = new Car("Clinker",55);
+            carArray[2] = new Car("Zippy",30);
+            carArray[3] = new Car("Fred",30);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return carArray.GetEnumerator();
+        }
+    }
+}
---
+++ CustomEnumerator/Program.cs
@@ -0,0 +1,10 @@
+﻿using CustomEnumerator;
+using System.Collections;
+
+Garage carLot =  new Garage();
+
+foreach (Car item in carLot)
+{
+    Console.WriteLine($"{item.PetName} is going {item.CurentSpeed} MPH");
+}
+
