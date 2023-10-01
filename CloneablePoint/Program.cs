7cc95eddb8c1eb368d4b796f1809179a7c128600
N1K1111 <nik.glazkov2016@yandex.ru>
2023-10-01 11:55:15 +0300

    1

---
+++ CloneablePoint/Program.cs
@@ -0,0 +1,24 @@
+﻿using CloneablePoint;
+
+
+
+Point p1 = new Point(50,50);
+Point p2 = p1;
+p2.X = 0;
+Console.WriteLine(p1);
+Console.WriteLine(p2);
+Console.ReadLine();
+
+Point p3 = new Point(100,100,"Jane");
+Point p4 = (Point)p3.Clone();
+
+Console.WriteLine("Перед модификацией:");
+Console.WriteLine($"p3 {p3}");
+Console.WriteLine($"p4 {p4}");
+p4.desc.PetName = "My new point";
+p4.X = 9;
+
+Console.WriteLine("После модификации:");
+Console.WriteLine($"p3 {p3}");
+Console.WriteLine($"p4 {p4}");
+Console.ReadLine();
---
+++ ComparableCar/Program.cs
@@ -0,0 +1,24 @@
+﻿using ComparableCar;
+
+
+
+Car[] myAutos = new Car[5];
+myAutos[0] = new Car("Rusti",80,1);
+myAutos[1] = new Car("Mary",40,234);
+myAutos[2] = new Car("Viper",40,34);
+myAutos[3] = new Car("Mel",40,4);
+myAutos[4] = new Car("Chucky",40,5);
+
+Array.Sort(myAutos, Car.SortByPetName);
+
+foreach (Car c in myAutos)
+{
+    Console.WriteLine(c.CarID);
+}
+
+
+Console.ReadLine();
+
+
+
+
