5d342f0a7d49b07dcd696bfe668d8cbc73f47c9b
N1K1111 <nik.glazkov2016@yandex.ru>
2023-10-01 11:55:15 +0300

    Add comparable

---
+++ CloneablePoint/PointDescription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    internal class PointDescription
+    {
+        public string PetName { get; set; }
+        public Guid PointID { get; set; }
+        public PointDescription() 
+        {
+            PetName = "No-Name";
+            PointID = Guid.NewGuid();
+        }
+    }
+}
